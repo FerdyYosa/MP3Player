69e928f80b405690b23d53c36a3efb993056efd4
Muhammad Ferdy Yosa <ferdyzecter@gmail.com>
2020-05-16 22:04:39 +0700

    Edit Project Again 2

--- app/src/main/java/com/example/mp3player/Mp3.java
+++ app/src/main/java/com/example/mp3player/Mp3.java
@@ -3,16 +3,16 @@ package com.example.mp3player;
 public class Mp3 {
     private long id;
     private String title;
-    private String artist;
+    private String mp32;
 
     public Mp3(long mp3ID, String mp3Title, String mp3) {
         id=mp3ID;
         title=mp3Title;
-        artist=mp3;
+        mp32=mp3;
     }
 
     public long getID(){return id;}
     public String getTitle(){return title;}
-    public String getArtist(){return artist;}
+    public String getArtist(){return mp32;}
 
 }
